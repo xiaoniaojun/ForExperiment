daff32cf20881c51b99c890a12505cc437a80c7d
xiaoniaojun <hackpoint@foxmail.com>
2016-07-13 16:19:51 +0800

    1

--- app/src/main/java/com/example/hackpoint/forexperiment/MainActivity.java
+++ app/src/main/java/com/example/hackpoint/forexperiment/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.hackpoint.forexperiment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn3 = (Button) findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Press Button3",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
 
